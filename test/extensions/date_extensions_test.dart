94c7c65422fb19fa207065fdd58f4aa559a09157
vycius <k.vycius@gmail.com>
2020-12-12 14:47:30 +0200

    Enable flutter test

---
+++ test/extensions/date_extensions_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import 'package:nephrolog/extensions/date_extensions.dart';
+
+void main() {
+  group('startAndEndOfWeek', () {
+    test('returns correct week start and end', () {
+      final today = DateTime.parse("2020-12-12T14:34:16.798918");
+      final startAndEnd = today.startAndEndOfWeek();
+      final start = startAndEnd.item1;
+      final end = startAndEnd.item2;
+
+      expect(start, DateTime.parse("2020-12-07T00:00:00.000"));
+      expect(end, DateTime.parse("2020-12-13 23:59:59.000"));
+    });
+  });
+}
