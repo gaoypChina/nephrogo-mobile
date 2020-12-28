b62fa6a9084df57e5915c33e99573af7f7f7dcc3
vycius <k.vycius@gmail.com>
2020-12-28 10:50:51 +0200

    Correct url for privacy and rules

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 class Constants {
   static const privacyPolicyUrl =
-      "https://www.nephrolog.lt/privatumo-politika/";
-  static const rulesUrl = "https://www.nephrolog.lt/privatumo-politika/";
+      "https://www.nephrolog.lt/documents/lt/privacy-policy.pdf";
+  static const rulesUrl =
+      "https://www.nephrolog.lt/documents/lt/fair-use-rules.pdf";
 }
