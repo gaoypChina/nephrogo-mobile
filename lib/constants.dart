a9ce798ff4f2c05256022d3030e5d06c3ae04b03
vycius <k.vycius@gmail.com>
2021-01-27 19:07:08 +0200

    Change e-mail

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ class Constants {
       "https://www.nephrogo.com/documents/lt/privacy-policy.pdf";
   static const rulesUrl =
       "https://www.nephrogo.com/documents/lt/fair-use-rules.pdf";
-  static const supportEmail = "nephrogo.info@gmail.com";
+  static const supportEmail = "info@nephrogo.com";
   static const supportPhone = "+37065253669";
 }
