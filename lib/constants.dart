bd2d9ea379df270142f08afc6119a7ed0a3e2ce5
vycius <k.vycius@gmail.com>
2021-02-04 18:12:16 +0200

    Change healthDataProcessingPolicyUrl

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 class Constants {
   static const privacyPolicyUrl =
       'https://www.nephrogo.com/documents/lt/privacy-policy.pdf';
-  static const healthDataProcessingPolicyUrl = privacyPolicyUrl;
+  static const healthDataProcessingPolicyUrl =
+      'https://www.nephrogo.com/documents/lt/processing-of-personal-health-data.pdf';
   static const rulesUrl =
       'https://www.nephrogo.com/documents/lt/fair-use-rules.pdf';
   static const supportEmail = 'info@nephrogo.com';
