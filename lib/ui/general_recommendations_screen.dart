de2457c0f3fa218f84e8d2fa28e5793394992165
vycius <k.vycius@gmail.com>
2021-03-09 14:00:34 +0200

    Change background for html

--- lib/ui/general_recommendations_screen.dart
+++ lib/ui/general_recommendations_screen.dart
@@ -180,14 +180,17 @@ class GeneralRecommendationScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.appLocalizations.generalRecommendations),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: HtmlWidget(
-          recommendation.body,
-          webView: true,
-          textStyle: Theme.of(context).textTheme.bodyText2.copyWith(
-                fontSize: 16,
-              ),
+      body: BasicSection.single(
+        margin: EdgeInsets.zero,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: HtmlWidget(
+            recommendation.body,
+            webView: true,
+            textStyle: Theme.of(context).textTheme.bodyText2.copyWith(
+                  fontSize: 16,
+                ),
+          ),
         ),
       ),
     );
