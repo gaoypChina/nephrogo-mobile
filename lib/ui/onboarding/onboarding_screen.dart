07e855b5dbc93035cef94f438b55f180aac57aa6
vycius <k.vycius@gmail.com>
2021-04-15 10:36:46 +0300

    Remove smooth_page_indicator

--- lib/ui/onboarding/onboarding_screen.dart
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -4,7 +4,6 @@ import 'package:nephrogo/preferences/app_preferences.dart';
 import 'package:nephrogo/routes.dart';
 import 'package:nephrogo/ui/analytics.dart';
 import 'package:nephrogo/ui/legal/legal_screen.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import 'onboarding_step.dart';
 
@@ -29,11 +28,11 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  static const stepsCount = 6;
+  static const _stepsCount = 6;
   final _controller = PageController(viewportFraction: 0.9999999);
-  int page = 0;
+  int _page = 0;
 
-  bool get isDone => page == (stepsCount - 1);
+  bool get _isDone => _page == (_stepsCount - 1);
 
   @override
   Widget build(BuildContext context) {
@@ -45,13 +44,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           leading: CloseButton(onPressed: close),
           backgroundColor: Colors.transparent,
           elevation: 0,
+          title: Text('${_page + 1} / $_stepsCount'),
+          centerTitle: true,
         ),
         body: Column(
           children: <Widget>[
             Expanded(
               child: PageView.builder(
                 controller: _controller,
-                itemCount: stepsCount,
+                itemCount: _stepsCount,
                 itemBuilder: (BuildContext context, int index) {
                   switch (index) {
                     case 0:
@@ -101,29 +102,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   }
                 },
                 onPageChanged: (int p) {
-                  setState(() {
-                    page = p;
-                  });
+                  setState(() => _page = p);
                 },
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8),
-              child: SmoothPageIndicator(
-                controller: _controller,
-                count: stepsCount,
-                effect: const ScaleEffect(
-                  dotColor: Colors.white,
-                  activeDotColor: Colors.white,
-                  scale: 2,
-                  dotWidth: 8,
-                  dotHeight: 8,
-                  radius: 8,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 16, 16, 32),
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 24),
               child: SizedBox(
                 width: double.infinity,
                 child: OutlinedButton(
@@ -131,11 +115,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     primary: Colors.black,
                     side: const BorderSide(width: 2, color: Colors.white),
                   ),
-                  onPressed: () => advancePageOrFinish(isDone: isDone),
+                  onPressed: () => advancePageOrFinish(isDone: _isDone),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      isDone
+                      _isDone
                           ? appLocalizations.finish.toUpperCase()
                           : appLocalizations.further.toUpperCase(),
                       style: Theme.of(context)
@@ -177,7 +161,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           break;
       }
     } else {
-      await _controller.animateToPage(page + 1,
+      await _controller.animateToPage(_page + 1,
           duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
     }
   }
