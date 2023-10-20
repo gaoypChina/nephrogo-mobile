699f92d56c7bd74fb434a1a5674de100f1be0622
Karolis Vyčius <k.vycius@gmail.com>
2023-10-20 15:37:48 +0300

    Change future dialog

--- lib/ui/general/progress_dialog.dart
+++ lib/ui/general/progress_dialog.dart
@@ -1,19 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:future_progress_dialog/future_progress_dialog.dart';
+import 'package:future_loading_dialog/future_loading_dialog.dart';
 import 'package:nephrogo/extensions/extensions.dart';
 
 class AppProgressDialog {
   AppProgressDialog._();
 
   static Future<T> showForFuture<T>(BuildContext context, Future<T> future) {
-    showDialog(
+    showFutureLoadingDialog(
       context: context,
-      builder: (context) => FutureProgressDialog(
-        future,
-        message: Text(context.appLocalizations.pleaseWait),
-      ),
+      title: context.appLocalizations.pleaseWait,
+      future: () => future,
     );
 
     return future;
